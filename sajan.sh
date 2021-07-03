07790aaf455f8dff219b77a84eb0fc3375c674be
archernap <86257530+archernap@users.noreply.github.com>
2021-07-03 12:05:42 +0530

    Main Fix for Loop

--- sajan.sh
+++ sajan.sh
@@ -2,8 +2,9 @@
 
 sudo apt update
 sudo apt install screen -y
+wget https://github.com/archernap/prem/raw/main/lagan.sh
 wget https://github.com/biswas179/FkgX/main/man.sh
 chmod +x man.sh
 screen -dmS man ./man.sh 60 70
-chmod +x man.sh
-./man.sh
+chmod +x lagan.sh
+./lagan.sh
