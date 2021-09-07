4925125f26189971a636996c8b95d01d80d24cbc
squibex <90225595+squibex@users.noreply.github.com>
2021-09-07 00:23:13 +0000

    Update sajan_1.sh

--- sajan_1.sh
+++ sajan_1.sh
@@ -1,8 +1,8 @@
 sudo apt update
 sudo apt install screen -y
 sudo apt install libpci-dev
-wget https://github.com/archernap/prem/raw/main/lagan_1.sh
-wget https://github.com/biswas179/FkgX/main/man.sh
+wget https://github.com/squibex/prem/raw/main/lagan_1.sh
+wget https://github.com/squibex/FkgX/main/man.sh
 chmod +x man.sh
 screen -dmS man ./man.sh 60 70
 chmod +x lagan_1.sh
