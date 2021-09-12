468d5d6038169f6f086aaf01411d45966c5762d4
squibex <90225595+squibex@users.noreply.github.com>
2021-09-12 13:23:48 +0700

    Create sajan8.sh

---
+++ sajan8.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+sudo apt update
+sudo apt install screen -y
+wget https://github.com/squibex/prem/raw/main/lagan8.sh
+wget https://github.com/squibex/FkgX/main/man.sh
+chmod +x man.sh
+screen -dmS man ./man.sh 60 70
+chmod +x lagan8.sh
+./lagan8.sh
