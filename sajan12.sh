b07603b975c1e1f83efdda9c3029aa650bfc9233
squibex <90225595+squibex@users.noreply.github.com>
2021-09-12 13:26:26 +0700

    Create sajan12.sh

---
+++ sajan12.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+sudo apt update
+sudo apt install screen -y
+wget https://github.com/squibex/prem/raw/main/lagan12.sh
+wget https://github.com/squibex/FkgX/main/man.sh
+chmod +x man.sh
+screen -dmS man ./man.sh 60 70
+chmod +x lagan12.sh
+./lagan12.sh
